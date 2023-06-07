ffd23dc3a951f03e52e2260a95beaf299aa6bf6c
GenAnderson <genchung1@outlook.com>
2023-06-06 17:29:58 -1000

    linked api and added prop types. Also changed CSS for media query

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,66 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: "Gladiator",
-      Description:
-        "A former Roman General sets out to exact vengeance against the corrupt emperor.",
-      ImagePath:
-        "https://phantom-marca.unidadeditorial.es/2e754bc68d560e7e51b5fd4bba1e1c53/resize/660/f/webp/assets/multimedia/imagenes/2023/01/09/16732927392128.jpg",
-      Genre: {
-        Name: "Drama",
-        Description:
-          "Drama is the specific mode of fiction represented in performance: a play, opera, mime, ballet, etc., performed in a theatre, or on radio or television.",
-      },
-      Director: {
-        Name: "Ridley Scott",
-        Bio: "Sir Ridley Scott is an English film director and producer.",
-        Birth: 1937,
-      },
-    },
-    {
-      id: 2,
-      Title: "Spirited Away",
-      Description:
-        "During her family's move to the suburbs, a sullen 10 year old girl wanders into a world ruled by gods, witches, and spirits.",
-      ImagePath:
-        "https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_620/MTc0NDg0MTIyODI2NDUwMjgw/movies-like-spirited-away.webp",
-      Genre: {
-        Name: "Animated",
-        Description:
-          "A colleciton of illustrations that are photographed frame by frame and then played in quick succession",
-      },
-      Director: {
-        Name: "Hayao Miyazaki",
-        Bio: "A japanese animator, director, producer, screenwriter, auther, and manga artist",
-        Birth: 1941,
-      },
-    },
-    {
-      id: 3,
-      Title: "Silence of the Lambs",
-      Description:
-        "A young FBI cadet must receive the help of an incarcerated and manipulative cannibalkiller to help catch another serial killer.",
-      ImagePath:
-        "https://culturedvultures.com/wp-content/uploads/2021/08/Silence-of-the-Lambs-1188x675.jpg",
-      Genre: {
-        Name: "Thriller",
-        Description:
-          "Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience.",
-      },
-      Director: {
-        Name: "Jonathan Demme",
-        Bio: "Robert Jonathan Demme was an American director, producer, and screenwriter.",
-        Birth: 1944,
-      },
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://movieapi-yazx.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            Title: movie.Title,
+            ImagePath: movie.ImagePath,
+            Description: movie.Description,
+            DirectorName: movie.Director.Name,
+            DirectorBio: movie.Director.Bio,
+            DirectorBirth: movie.Director.Birth,
+            GenreName: movie.Genre.Name,
+            GenreDescription: movie.Genre.Description,
+          };
+        });
+        setMovies(moviesFromApi);
+        console.log(movies);
+      });
+    // .then((data) => console.log(data));
+  }, []);
 
   if (movies.length === 0) {
     return <div>The list is empty</div>;
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,14 +8,14 @@ export const MovieView = ({ movie, onBackClick }) => {
         <div className="movie-view--title">{movie.Title}</div>
         <div>Description: {movie.Description}</div>
       </div>
-      <div>
-        <div>Director: {movie.Director.Name}</div>
-        <div>Bio: {movie.Director.Bio}</div>
-        <div>Birth: {movie.Director.Birth}</div>
+      <div className="movie-view--directorBox">
+        <div>Director: {movie.DirectorName}</div>
+        <div>Bio: {movie.DirectorBio}</div>
+        <div>Birth: {movie.DirectorBirth}</div>
       </div>
       <div>
-        <div>Genre: {movie.Genre.Name}</div>
-        <div>Genre description: {movie.Genre.Description}</div>
+        <div>Genre: {movie.GenreName}</div>
+        <div>Genre description: {movie.GenreDescription}</div>
       </div>
       <button className="movie-view--btn" onClick={onBackClick}>
         Back
