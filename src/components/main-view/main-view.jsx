40989db392a58d216b88082dba1a41358d03113e
GenAnderson <genchung1@outlook.com>
2023-06-13 15:08:18 -1000

    added login, signup, and styled

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignUpView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
 
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("https://movieapi-yazx.onrender.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movieapi-yazx.onrender.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
@@ -25,13 +38,21 @@ export const MainView = () => {
           };
         });
         setMovies(moviesFromApi);
-        console.log(movies);
       });
-    // .then((data) => console.log(data));
-  }, []);
+  }, [token]);
 
-  if (movies.length === 0) {
-    return <div>The list is empty</div>;
+  if (!user) {
+    return (
+      <div className="loginSignupContainer">
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />{" "}
+        <p>OR</p> <SignUpView />
+      </div>
+    );
   }
 
   if (selectedMovie) {
@@ -47,17 +68,34 @@ export const MainView = () => {
     );
   }
 
+  if (movies.length === 0) {
+    return <div>The list is empty</div>;
+  }
+
   return (
-    <div className="main-view">
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
+    <div>
+      <div className="main-view">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </div>
+      <div className="main-view--logoutBtnContainer">
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
           }}
-        />
-      ))}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
